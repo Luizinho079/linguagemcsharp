5e0a241b2c4365280deac417457ffffaa9270129
Luizinho079 <115711699+Luizinho079@users.noreply.github.com>
2022-11-24 11:35:45 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,23 +1,37 @@
 ﻿using System;
 
-namespace TipoByte
+namespace tipos_de_variaveis
 {
     class Program
     {
         static void Main(string[] args)
         {
-            try
+            int num1 = 5;
+            int num2 = num1;
+            int resultado = num1 + num2;
+            num1 = 6;
+
+            Console.WriteLine("Valor do num1 é: {0}  e está na stack", num1.ToString());
+            Console.WriteLine("Valor do num2 é: {0}  e está na stack", num2.ToString());
+
+            Console.WriteLine("Calculadora");
+            Calculadora minhaCalculadora = new Calculadora();
+            minhaCalculadora.somar()
+
+            Console.ReadKey());
 
 
-                byte x = Convert.ToByte(Console.ReadLine());
-                Console.WriteLine("Os valores digitados são: {0}, x)", x);
 
-            catch (OverFlowException)
 
-               Console.WriteLine("O valor digitado esta fora o máximo. O máximo é 255");
 
-            catch (FormaException)
 
-               Console.WriteLine("Você digitou um texto. Só é aceitável um número de 0 a 255");
+
+
+        }
+
+        
     }
+           
+          
+          
 }
