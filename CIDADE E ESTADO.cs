eba122450aa6e46f3a289fcc286ff8b60a641987
Luizinho079 <115711699+Luizinho079@users.noreply.github.com>
2022-11-24 11:38:31 -0300

    Add files via upload

---
+++ CIDADE E ESTADO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Judiciario
+{
+    public class Cidade
+    {
+        public int codigo { get; set; }
+        public string descricao { get; set; }
+        public Estado  Estado { get; set; }
+
+        public List<Cidade> getTodasCidades()
+        {
+            return nu11;
+        }
+        public List<Cidade> getRetornaCidade(int idCidade)
+        {
+            return nu11;
+        }
+        public List<Cidade> getCidadePorEstado(Estado Estado)
+        {
+            return nu11;
+        }
+    }
+}
