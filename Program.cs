e646fce2712a86404b30f87776e0b733b11523fe
Luizinho079 <115711699+Luizinho079@users.noreply.github.com>
2022-11-24 11:34:54 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,33 +1,32 @@
 ﻿using System;
 
-namespace EleiçãoWrite
+namespace ListaByte
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Eleições 2022");
-            int qtdCand26 = 0;
-            int qtdCand44 = 0;
-            int qtdCand56 = 0;
-            int qtdNullo = 0;
-            bool continuar = true;
-            while (continuar)
-                Console.WriteLine("Qual seu nome: ");
-                string nome = Console.ReadLine();
-                Console.Write("Vote em um dos candidatos a seguir: ");
-                Console.Write("26 - João ");
-                Console.Write("44 - Maria");
-                Console.Write("56 - Tabet");
-                Console.Write("0 - Nulo");
-                Console.Write("Só digite o número");
-            int meuVoto = Convert.ToInt32(Console.ReadLine() );
-            
-            if (meuVoto==26) { qtdCand26 = qtdCand26 + 1: }
-            else if (meuVoto --44) { qtdCand44 = qtdCand44 + 1;
-            else if (meuVoto --56) { qtdCand56 = qtdCand56 + 1:
-            else qtdNullo = }
-             
+            object[] valores = { 4, "gota", 300, 500, 3 };
+            byte[] vbyte = new byte[5];
+            string[] vtextos = new string[5];
+            string[] vforafaixa = new string[5];
+
+            for (int i =0; i< valores.Length; i++)
+            {
+                try
+                {
+                    var v = Convert.ToByte(valores[i];
+                    vbytes[i] = v;
+                }
+                catch (FormatException)
+                {
+                    vtextos[i] = valores[i].ToString();
+
+                }
+                catch (OverFlowException)
+                {
+                    vforafaixa[i] = Convert.ToInt32(valores[i]);
+                }
         }
     }
 }
