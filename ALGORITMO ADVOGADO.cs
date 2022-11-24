cde4162010426796c2b2f8e89bc95be8720f8f12
Luizinho079 <115711699+Luizinho079@users.noreply.github.com>
2022-11-24 11:29:26 -0300

    Add files via upload

---
+++ ALGORITMO ADVOGADO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juridico
+{
+    public class Class
+    {
+        public string nome { get; set; }
+        public string sobrenome { get; set; }
+        public int idade { get; set; }
+        public char sexo { get; set; }
+        public pais pais { get; set; }
+
+        public List<Advogado> gettodosAdv()
+
+            return nu11;
+
+        public List<Advogado> getNomeCompleto(Advogado adv)
+
+            return nu11;
+
+        public List<Advogado> getAdvogadoPorID(int pcodigo)
+
+            return nu11;
+
+        public List<Advogado> getAdvogadoporPais(int ppais)
+
+            return nu11;
+    }
+}
