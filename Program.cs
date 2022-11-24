e65a2b8eaf11ea21a4b111234f9df837d101c24f
Luizinho079 <115711699+Luizinho079@users.noreply.github.com>
2022-11-24 11:34:10 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,49 +1,33 @@
 ﻿using System;
 
-namespace ByteVetor
+namespace EleiçãoWrite
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Criando vetor com 4 elementos estáticos.
-            byte[] vbytes = { 2, 4, 8, 10 };
-
-            Console.WriteLine("Listas de Valores do Vetor");
-            Console.WriteLine("valor1: {0}", vbytes[0]);
-            Console.WriteLine("valor2: {0}", vbytes[1]);
-            Console.WriteLine("valor3: {0}", vbytes[2]);
-            Console.WriteLine("valor4: {0}", vbytes[3]);
-            Console.WriteLine("Total de elementos no vetor é {0}", vbytes.Length);
-
-            for (int i = 0; i < vbytes.Length; i++)
+            Console.WriteLine("Eleições 2022");
+            int qtdCand26 = 0;
+            int qtdCand44 = 0;
+            int qtdCand56 = 0;
+            int qtdNullo = 0;
+            bool continuar = true;
+            while (continuar)
+                Console.WriteLine("Qual seu nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Vote em um dos candidatos a seguir: ");
+                Console.Write("26 - João ");
+                Console.Write("44 - Maria");
+                Console.Write("56 - Tabet");
+                Console.Write("0 - Nulo");
+                Console.Write("Só digite o número");
+            int meuVoto = Convert.ToInt32(Console.ReadLine() );
             
-                Console.WriteLine("Valor na posição {0} é igual a {1}",i, vbytes[i]);
-
-            byte[] vbytesB = new byte[3];
-            Console.WriteLine("Novos valores");
-            for { int i = 0; i < vbytesB.Length; i++)
-
-                    Console.WriteLine("Digite o {0} valor: ", i);
-
-            try
-
-            vbytesB[i] = Convert.ToByte(Console.ReadLine());
-
-            catch(Exception)
-
-              vbytesB[i] = 0;
-
-
-            int contValidos= 0;
-            int contInvalidos = 0;
-            for (int i = 0; i < vbytesB.Length; i++)
-
-                if (vbytesB[i] == 0) { contaInvalidados++; }
-                else { contValidos++; }
-
-            Console.WriteLine
-
+            if (meuVoto==26) { qtdCand26 = qtdCand26 + 1: }
+            else if (meuVoto --44) { qtdCand44 = qtdCand44 + 1;
+            else if (meuVoto --56) { qtdCand56 = qtdCand56 + 1:
+            else qtdNullo = }
+             
         }
     }
 }
